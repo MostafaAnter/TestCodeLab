6616d94e7d7b6e7e6bd6f40c76eb1776a42b39d9
MostafaAnter <ga30000@gmail.com>
2020-11-06 23:51:59 +0200

    Initial commit

---
+++ app/src/main/java/com/example/testcodelab/RegistrationUtil.kt
@@ -0,0 +1,14 @@
+package com.example.testcodelab
+
+/**
+ * Created by Mostafa Anter on 11/6/20.
+ */
+object RegistrationUtil{
+    fun validateRegistrationInput(
+        userName: String,
+        password: String,
+        confirmedPassword: String
+    ): Boolean{
+        return true
+    }
+}
---
+++ app/src/test/java/com/example/testcodelab/RegistrationUtilTest.kt
@@ -0,0 +1,77 @@
+package com.example.testcodelab
+
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+/**
+ * Created by Mostafa Anter on 11/6/20.
+ */
+class RegistrationUtilTest{
+
+    @Test
+    fun `empty username returns false`(){
+        val result = RegistrationUtil.validateRegistrationInput(
+            "",
+            "123",
+            "123"
+        )
+
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `valid username and correctly repeated password returns true`(){
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Philipp",
+            "123",
+            "123"
+        )
+
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `username already exists returns false`(){
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Carl",
+            "123",
+            "123"
+        )
+
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `empty password returns false`(){
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Carl",
+            "",
+            ""
+        )
+
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `password was repeated incorrectly returns false`(){
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Carl",
+            "123",
+            "132"
+        )
+
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `password contains less than 2 digits returns false`(){
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Carl",
+            "adsfdsaf1",
+            "adsfdsaf1"
+        )
+
+        assertThat(result).isFalse()
+    }
+}
